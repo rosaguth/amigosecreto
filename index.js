ffd216a45cdbef8b14f6175115be14ede0964762
rosaguth <162591122+rosaguth@users.noreply.github.com>
2025-08-26 10:13:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let amigos = [];
-let amigo=[];document.getElementById(enviar).onclick = function(){
-    let amigo = document.getElementById('nomes').value.trim();
-    if(amigo){
-     amigos.push(amigo)
-        for(let i = 0; i <amigos.length; i++){
-            document.getElementById('lista').innerHTML = `${(amigos)[i]}`
-        }
+    document.getElementById('enviar').onclick = function(){
+        let amigo = document.getElementById('nomes').value.trim()
+        if(amigo){
+            amigos.push(amigo)
+           document.getElementById('nomes').value = '';
+            document.getElementById('lista').innerHTML = ' ';
+            for( let i = 0; i < amigos.length; i++){
+                document.getElementById('lista').innerHTML += `<li>${amigos[i]}`
+            }
         }
     }
+document.getElementById('sortear').onclick =function(){
+    let sorteado = amigos[Math.floor(Math.random()* amigos.length)]
+    document.getElementById('lista2').innerHTML = `<li> o sorteado é : ${sorteado}</li>`
+}
